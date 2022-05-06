8b44101e8cbbf9031a453687b2f9085023840c48
Jason L <jkleitz27@gmail.com>
2022-05-06 13:51:09 -0400

    showing date

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Show today's date
+$(document).ready(function(){
+    //current time
+    $("#currentDay").text(moment().format("dddd, MMM Do YYYY "));
+    // $(.saveBtn).on("click", function(){
+
+    // })
+});
