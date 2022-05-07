3af807c87ce2b598cd5830f4763813627855e579
Jason L <jkleitz27@gmail.com>
2022-05-06 20:26:03 -0400

    time colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,35 @@
 $(document).ready(function(){
     //current time
     $("#currentDay").text(moment().format("dddd, MMM Do YYYY "));
-    // $(.saveBtn).on("click", function(){
-
-    // })
+    
 });
+
+//track the time
+
+function timeTracker (){
+    // get current time
+    var currentTime = moment().hour();
+    
+    //loop for schedule blocks
+    $(".time-block").each(function(){
+        var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+        //check for time and add classes for background
+        if (hourBlock < currentTime){
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (hourBlock === currentTime){
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+        else{
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    })
+};
+timeTracker();
